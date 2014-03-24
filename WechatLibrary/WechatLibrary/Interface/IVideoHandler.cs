1da232b6d2c2e7b36bf0f6b760bf4bb81cffe396
h82258652 <842053625@qq.com>
2014-03-24 17:14:42 +0800

    update
    更新接口。

---
+++ WechatLibrary/WechatLibrary/Interface/IVideoHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WechatLibrary.Message.Request;
+using WechatLibrary.Message.Response;
+
+namespace WechatLibrary.Interface
+{
+    public interface IVideoHandler
+    {
+        ResponseResultBase ProcessRequest(VideoMessage message);
+    }
+}
