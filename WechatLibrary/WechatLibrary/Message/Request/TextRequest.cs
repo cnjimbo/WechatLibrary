8b83f91b7ae1cccd848c2ce956e7e137e569bfb3
h82258652 <842053625@qq.com>
2014-03-24 15:53:32 +0800

    update

---
+++ WechatLibrary/WechatLibrary/Message/Request/TextRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WechatLibrary.Message.Request
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TextRequest : RequestBase
+    {
+        /// <summary>
+        /// 文本消息内容。
+        /// </summary>
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 消息id，64位整型。
+        /// </summary>
+        public long MsgId
+        {
+            get;
+            set;
+        }
+    }
+}
