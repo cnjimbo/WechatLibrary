8502e7177f81db9618aba8cd3c9f29ae0bc6584e
h82258652 <842053625@qq.com>
2014-03-24 15:39:38 +0800

    update
    1、更改为.net 4.0；
    2、开启生成xml文档。

---
+++ WechatLibrary/WechatLibrary/Wechat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WechatLibrary
+{
+    /// <summary>
+    /// 微信类库入口。
+    /// </summary>
+    public class Wechat
+    {
+        public static void ProcessRequest()
+        {
+        }
+    }
+}
