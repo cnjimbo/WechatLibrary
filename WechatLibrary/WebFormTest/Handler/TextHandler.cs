45a1c718c7eeda855998483094a08b387a3d677c
h82258652 <842053625@qq.com>
2014-03-28 17:26:03 +0800

    update
    更新。

---
+++ WechatLibrary/WebFormTest/Handler/TextHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WechatLibrary.Interface;
+using WechatLibrary.Request;
+using WechatLibrary.Response;
+
+namespace WebFormTest.Handler
+{
+    public class TextHandler : ITextHandler
+    {
+        public ResponseResultBase ProcessRequest(TextMessage message, ref bool dbProcess)
+        {
+            dbProcess = true;
+            return new TextResult()
+            {
+                Content = "Haha"
+            };
+        }
+    }
+}
