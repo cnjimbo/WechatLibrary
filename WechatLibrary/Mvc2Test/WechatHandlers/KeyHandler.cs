39ff1b2f45217a13ad7b85d7cf310171b6f3e514
h82258652 <842053625@qq.com>
2014-04-17 10:05:29 +0800

    update
    it is a useable version for .net 4.0(vs2010) and .net 4.5(vs2013).

---
+++ WechatLibrary/Mvc2Test/WechatHandlers/KeyHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WechatLibrary.Interface;
+using WechatLibrary.Response;
+
+// 请修改命名空间起始为数据库中注册的命名空间。
+namespace TTTWX.Mvc2Test.WechatHandlers
+{
+    // 请实现相应的接口，对于菜单点击事件，请将类命名为Key+Handler，其中Key不区分大小写。
+    public class KeyHandler : IMenuButtonHandler
+    {
+        public WechatLibrary.Response.ResponseResultBase ProcessRequest(WechatLibrary.Request.MenuButtonMessage message, ref bool dbProcess)
+        {
+            return new TextResult()
+            {
+                Content = "this is test"
+            };
+        }
+    }
+}
---
+++ WechatLibrary/Mvc4Test.bak/WechatHandlers/TextHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WechatLibrary.Interface;
+using WechatLibrary.Response;
+
+// 请修改命名空间起始为数据库中注册的命名空间。
+namespace TTTWX.Mvc4Test.WechatHandlers
+{
+    // 请实现相应的接口。
+    public class TextHandler : ITextHandler
+    {
+        public WechatLibrary.Response.ResponseResultBase ProcessRequest(WechatLibrary.Request.TextMessage message, ref bool dbProcess)
+        {
+            return new TextResult()
+            {
+                Content = "this is a test"
+            };
+        }
+    }
+}
